0ca5c4aa68285b91ceab833a3bc1d2e191631e33
Derrick D. Young <youngderrick82@gmail.com>
2023-04-15 10:42:45 -0500

    Class items

---
+++ 4:12:23/index.js
@@ -0,0 +1,21 @@
+
+
+function helloWorld() {
+    console.log("hello world")
+}
+
+
+const firstName = "Derrick";
+const lastName = "Young"
+function helloToYou(firstName, lastName) {
+   return  "hello to you, " + firstName + " " + lastName
+
+
+}
+
+helloWorld();
+
+console.log(firstName);
+
+const message = helloToYou("Lexie", "Young");
+
